afdf14f54806b2cea28b0bb9ae65bffc28e8baed
Paula Ibieta <paula.ibieta@gmail.com>
2016-05-11 11:37:51 -0700

    added panels and error messages

--- app/models/concerns/yahoo_api.rb
+++ app/models/concerns/yahoo_api.rb
@@ -2,7 +2,7 @@ class YahooApi
 
   def self.response tag
     yahoo_client = YahooFinance::Client.new
-    data = yahoo_client.quotes(["#{tag}"], [:ask, :bid, :change, :market_capitalization, :day_value_change, :earnings_per_share])
+    data = yahoo_client.quotes(["#{tag}"], [:ask, :bid, :change, :market_capitalization, :day_value_change, :earnings_per_share, :name])
   end 
 
 end 
